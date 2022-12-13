ef53d46c258348028052dd1697b889f8e01b513a
kimdowon <101384265+kkdo11@users.noreply.github.com>
2022-12-13 14:44:01 +0900

    Delete src/main/java/kopo/poly/persistance directory

--- src/main/java/kopo/poly/persistance/mapper/IMovieMapper.java
+++
@@ -1,9 +0,0 @@
-package kopo.poly.persistance.mapper;
-
-import kopo.poly.dto.MovieDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IMovieMapper {
-    int InsertMovieInfo(MovieDTO pDTO) throws Exception;
-}
